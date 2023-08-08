9a719a8669e38c6eebe77d7be11fdb7527f0c1ed
Elivelton Souza <67668057+EliveltonSouzaDev@users.noreply.github.com>
2023-08-07 23:37:51 -0300

    adiciona tratamento de erros e filtro por categoria

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const Product = require("./config.js");
+const format = require("date-fns/format");
 const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,37 +11,68 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", async (req, res) => {
     try {
         const snapshot = await Product.get();
-        const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        res.send(list);
+
+        if (snapshot.empty) {
+            res.status(404).send({ msg: "No products found" });
+            return;
+        }
+
+        const productList = [];
+        snapshot.forEach((doc) => {
+            productList.push({ id: doc.id, ...doc.data() });
+        });
+
+        res.send(productList);
     } catch (error) {
-        res.send({ msg: error });
+        res.status(500).send({ msg: error });
     }
 });
 
 app.get("/:id", async (req, res) => {
     try {
-        const snapshot = await Product.get();
-        const list = snapshot.docs.filter((doc) => (doc.id === req.params.id)).map((doc) => ({ id: doc.id, ...doc.data() }));
-        res.send(list);
+        const productId = req.params.id;
+
+        const productRef = Product.doc(productId);
+        const productDoc = await productRef.get();
+
+        if (!productDoc.exists) {
+            res.status(404).send({ msg: "Product to add not found" });
+            return;
+        }
+
+        const productData = productDoc.data();
+        const productWithId = { id: productDoc.id, ...productData };
+
+        res.send(productWithId);
     } catch (error) {
-        res.send({ msg: error });
+        res.status(500).send({ msg: error });
     }
 });
 
-//pendente get by category
-app.get("/getByCategory", async (req, res) => {
-    const category = req.body.category;
+app.get("/filter/byCategory", async (req, res) => {
     try {
-        const snapshot = await Product.get();
-        const list = snapshot.doc(category).data();
-        res.send(list);
+        const category = req.body.category;
+        const querySnapshot = await Product.where("category", "==", category).get();
+
+        if (querySnapshot.empty) {
+            res.status(404).send([{ msg: "No products found to category" }]);
+        } else {
+            const products = [];
+            querySnapshot.forEach((doc) => {
+                products.push(doc.data());
+            });
+            res.send(products);
+        }
     } catch (error) {
-        res.send({ msg: error });
+        res.status(500).send({ msg: "An error occurred" });
     }
 });
 
 app.post("/add", async (req, res) => {
     try {
+        const currentDate = new Date();
+        const formattedDateTime = format(currentDate, "MM/dd/yyyy HH:mm:ss");
+
         const productJson = {
             walletId: req.body.walletId,
             title: req.body.title,
@@ -48,12 +81,12 @@ app.post("/add", async (req, res) => {
             price: req.body.price,
             images: req.body.images,
             category: req.body.category,
-            createdAt: Date.now(),//revisar como salvar a data
+            createdAt: formattedDateTime,
         };
         await Product.add(productJson);
         res.send({ msg: "Product added to database" });
     } catch (error) {
-        res.send({ msg: error });
+        res.status(500).send({ msg: error });
     }
 });
 
@@ -62,23 +95,47 @@ app.post("/update", async (req, res) => {
         const id = req.body.id;
         delete req.body.id;
         const data = req.body;
-        await Product.doc(id).update(data);
-        res.send({ msg: "Updated" });
-    } catch (error) {
-        res.send({ msg: error });
-    }
 
+        data.updatedAt = format(new Date(), "MM/dd/yyyy HH:mm:ss");
+
+        const productRef = Product.doc(id);
+
+        const currentProductDoc = await productRef.get();
+        if (!currentProductDoc.exists) {
+            res.send({ msg: "Product not found" });
+            return;
+        }
+
+        await productRef.update(data);
+
+        const updatedProductDoc = await productRef.get();
+        const updatedProductData = updatedProductDoc.data();
+
+        res.send({ msg: "Updated", updatedProduct: updatedProductData });
+    } catch (error) {
+        res.status(500).send({ msg: error });
+    }
 });
 
 app.delete("/delete", async (req, res) => {
     try {
         const id = req.body.id;
-        await Product.doc(id).delete();
-        res.send({ msg: "Deleted" });
-    } catch (error) {
-        res.send({ msg: error });
-    }
+        const deletedProductRef = Product.doc(id);
 
+        const deletedProductDoc = await deletedProductRef.get();
+        if (!deletedProductDoc.exists) {
+            res.status(404).send({ msg: "Product not found" });
+            return;
+        }
+
+        const deletedProductData = deletedProductDoc.data();
+
+        await deletedProductRef.delete();
+
+        res.send({ msg: "Deleted", deletedProduct: deletedProductData });
+    } catch (error) {
+        res.status(500).send({ msg: error });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
