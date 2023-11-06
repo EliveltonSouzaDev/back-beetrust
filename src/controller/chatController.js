262e11e465c5b18843a1d3886b6e71255db23bb6
EliveltonSouzaDev <tst.elivelton@gmail.com>
2023-11-05 22:19:16 -0300

    Adicionando rotas chat

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -3,18 +3,19 @@ const { Chat } = require("../configs/config.js");
 
 const sendMessage = async (req, res) => {
     try {
-        const { senderId, receiverId, message } = req.body;
+        const { senderId, receiverId, message, chatId } = req.body;
 
         const orderedIds = [senderId, receiverId].sort();
 
-        const chatId = orderedIds[0] + "_" + orderedIds[1];
+        const newChatId = chatId + "_" + orderedIds[0] + "_" + orderedIds[1];
 
-        const chatDoc = await Chat.doc(chatId).get();
+        const chatDoc = await Chat.doc(newChatId).get();
 
         if (!chatDoc.exists) {
             const newChatData = {
                 senderId,
                 receiverId,
+                newChatId,
                 messages: [
                     {
                         senderId,
@@ -25,7 +26,7 @@ const sendMessage = async (req, res) => {
                 ]
             };
 
-            await Chat.doc(chatId).set(newChatData);
+            await Chat.doc(newChatId).set(newChatData);
         } else {
             // Se o chat existir, adicione a nova mensagem às mensagens existentes no chat
             const chatData = chatDoc.data();
@@ -36,7 +37,7 @@ const sendMessage = async (req, res) => {
                 createdAt: new Date(),
             });
 
-            await Chat.doc(chatId).update({
+            await Chat.doc(newChatId).update({
                 messages: chatData.messages
             });
         }
@@ -51,7 +52,7 @@ const sendMessage = async (req, res) => {
 
 const getUserMessages = async (req, res) => {
     try {
-        const userId = req.params.userId;
+        const { userId } = req.body;
         const userMessages = [];
 
         const chatQuerySnapshot = await Chat.get();
@@ -59,24 +60,74 @@ const getUserMessages = async (req, res) => {
         for (const chatDoc of chatQuerySnapshot.docs) {
             const chatId = chatDoc.id;
 
+            // Access the 'messages' array from the document data
+            const messagesArray = chatDoc.data().messages;
+
+            // Initialize variables for the latest messages
+            let latestMessage = null;
+
+            for (const message of messagesArray) {
+                const createdAtTimestamp = message.createdAt.toDate();
+                const senderId = message.senderId;
+                const receiverId = message.receiverId;
+                const messageContent = message.message;
+
+                // Logic to determine the latest message within the 'messages' array
+                if (
+                    !latestMessage ||
+                    createdAtTimestamp > latestMessage.createdAt
+                ) {
+                    latestMessage = {
+                        createdAt: createdAtTimestamp,
+                        senderId,
+                        receiverId,
+                        messageContent,
+                    };
+                }
+            }
+
             const messageQuerySnapshot = await chatDoc.ref
-                .collection("messages")
-                .where("sender", "==", userId)
+                .collection("Chat")
+                .where("senderId", "==", userId)
                 .orderBy("createdAt", "desc")
                 .limit(1)
                 .get();
 
             const receivedMessagesQuerySnapshot = await chatDoc.ref
-                .collection("messages")
-                .where("receiver", "==", userId)
+                .collection("Chat")
+                .where("receiverId", "==", userId)
                 .orderBy("createdAt", "desc")
                 .limit(1)
                 .get();
 
-            const lastSentMessage = messageQuerySnapshot.empty ? null : messageQuerySnapshot.docs[0].data();
-            const lastReceivedMessage = receivedMessagesQuerySnapshot.empty ? null : receivedMessagesQuerySnapshot.docs[0].data();
+            const lastSentMessage = messageQuerySnapshot.empty
+                ? null
+                : messageQuerySnapshot.docs[0].data();
 
-            const latestMessage = lastReceivedMessage && (!lastSentMessage || lastReceivedMessage.createdAt > lastSentMessage.createdAt) ? lastReceivedMessage : lastSentMessage;
+            const lastReceivedMessage = receivedMessagesQuerySnapshot.empty
+                ? null
+                : receivedMessagesQuerySnapshot.docs[0].data();
+
+            // Determine the latest of the sent and received messages
+            if (
+                lastReceivedMessage &&
+                (!lastSentMessage ||
+                    lastReceivedMessage.createdAt > lastSentMessage.createdAt)
+            ) {
+                latestMessage = {
+                    createdAt: lastReceivedMessage.createdAt,
+                    senderId: lastReceivedMessage.senderId,
+                    receiverId: lastReceivedMessage.receiverId,
+                    messageContent: lastReceivedMessage.message,
+                };
+            } else if (lastSentMessage) {
+                latestMessage = {
+                    createdAt: lastSentMessage.createdAt,
+                    senderId: lastSentMessage.senderId,
+                    receiverId: lastSentMessage.receiverId,
+                    messageContent: lastSentMessage.message,
+                };
+            }
 
             if (latestMessage) {
                 userMessages.push({
@@ -93,24 +144,27 @@ const getUserMessages = async (req, res) => {
     }
 };
 
+
 const getChatMessages = async (req, res) => {
     try {
-        const chatId = req.params.chatId;
-        const chatMessages = [];
+        const { chatId } = req.body;
 
-        const chatRef = Chat.doc(chatId);
-        const messageQuerySnapshot = await chatRef.collection("messages")
-            .orderBy("createdAt", "asc")
-            .get();
+        const chatDoc = await Chat.doc(chatId).get();
 
-        messageQuerySnapshot.forEach((messageDoc) => {
-            chatMessages.push(messageDoc.data());
-        });
+        if (!chatDoc.exists) {
+            res.status(404).send({ msg: "Chat not found" });
+            return;
+        }
 
-        res.send(chatMessages);
+        const messagesArray = chatDoc.data().messages;
+
+        // Sort the messages by createdAt timestamp in ascending order
+        const sortedMessages = messagesArray.sort((a, b) => a.createdAt - b.createdAt);
+
+        res.send(sortedMessages);
     } catch (error) {
-        res.status(500).send({ msg: error });
-        throw error;
+        console.error("Error:", error);
+        res.status(500).send({ msg: error.message });
     }
 };
 
